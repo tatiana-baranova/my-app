5515482270dcbdb0632eec4a8d2182597176fad5
tetiana_baranova <syniovych.tanya@gmail.com>
2025-06-23 23:44:06 +0300

    tailwindcss init

--- src/app/components/not-active/NotActiveLabel.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import s from './NotActiveLabel.module.css';
-export interface NotActiveLabelProps {
-  children: React.ReactNode;
-}
-export default function NotActiveLabel({ children }: NotActiveLabelProps) {
-  return <span className={s.label}>{children}</span>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import ActiveLabel from './components/active-label/ActiveLabel';
-import NotActiveLabel from './components/not-active/NotActiveLabel';
+import StatusLabel, { Status } from './components/status-label/StatusLabel';
 
 export default function Home() {
   return (
     <div>
       <main>
-        <h1>Home Page</h1>
-        <ActiveLabel>Active</ActiveLabel>
-        <NotActiveLabel>Not active</NotActiveLabel>
+        <h1 className="text-xl">Home Page</h1>
+        <StatusLabel status={Status.Active}>Active</StatusLabel>
+        <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+        <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+        <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
       </main>
       {/* <footer>
         <h2>Footer</h2>
