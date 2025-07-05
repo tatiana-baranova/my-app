e7a6553486fd03b92302f9719ffda26255e4266b
tetiana_baranova <syniovych.tanya@gmail.com>
2025-07-05 22:03:29 +0300

    dynamic rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { headers } from 'next/headers';
 import StatusLabel, { Status } from './components/status-label/StatusLabel';
 
-export default function Home() {
+export default async function Home() {
+  const headersList = await headers();
+  const userAgent = headersList.get('user-agent');
+  console.log('User-Agent:', userAgent);
   return (
     <div>
       <main>
-        <h1 className="text-xl">Home Page</h1>
+        <h1 className="text-xl">Home Page {new Date().toTimeString()} </h1>
         <StatusLabel status={Status.Active}>Active</StatusLabel>
         <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
         <StatusLabel status={Status.Pending}>Pending</StatusLabel>
